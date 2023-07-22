44133dc03c3d2afb79fac6fb8ee0ae85acf29020
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-07-22 23:11:02 +0530

    not completed

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const startTimerBtn = document.getElementById('start-btn');
+
+startTimerBtn.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const hours = parseInt(document.getElementById('hour').value);
+    const minutes = parseInt(document.getElementById('minute').value);
+    const seconds = parseInt(document.getElementById('second').value);
+
+})
